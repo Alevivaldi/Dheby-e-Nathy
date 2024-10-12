c3665867ae3c7d58494cf1db50e14f481588210e
Alex Costa <thg6321@gmail.com>
2024-10-12 03:27:13 -0300

    v2

--- src/main/java/br/com/alexcosta/controllers/ClienteControllers.java
+++ src/main/java/br/com/alexcosta/controllers/ClienteControllers.java
@@ -2,10 +2,12 @@ package br.com.alexcosta.controllers;
 import br.com.alexcosta.dto.CadastroDTO;
 import br.com.alexcosta.dto.ClienteDTO;
 import br.com.alexcosta.entities.Cliente;
-import br.com.alexcosta.services.ClienteServices;
+import br.com.alexcosta.entities.Endereco;
+import br.com.alexcosta.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
 
 @CrossOrigin(origins = "http://localhost:8080") 
 
@@ -14,16 +16,35 @@ import org.springframework.web.bind.annotation.*;
 public class ClienteControllers {
 
     @Autowired
-    private ClienteServices clienteServices;
+    private ClienteRepository clienteRepository;
 
     @PostMapping(value="/login")
     public boolean login(@RequestBody ClienteDTO dto) {
-        return clienteServices.login(dto.getUsername(), dto.getPassword());
+        if (dto.getUsername() == null || dto.getPassword() == null) {
+            throw new IllegalArgumentException("Usuário ou senha não podem ser nulos");
+        }
+
+        Cliente user = clienteRepository.findByUsername(dto.getUsername());
+        if (user == null) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+        return dto.getPassword().equals(user.getPassword());
     }
     @PostMapping(value="/cadastro")
-    public ResponseEntity<Cliente> cadastrar(@RequestBody CadastroDTO cadastroDTO) {
-        Cliente cliente = clienteServices.cadastrar(cadastroDTO);
-        return ResponseEntity.ok(cliente);
+    public Cliente cadastrar(@RequestBody CadastroDTO cadastroDTO) {
+        Endereco endereco = new Endereco();
+        endereco.setCep(cadastroDTO.getEndereco().getCep());
+        endereco.setLogradouro(cadastroDTO.getEndereco().getLogradouro());
+        endereco.setCidade(cadastroDTO.getEndereco().getCidade());
+        endereco.setEstado(cadastroDTO.getEndereco().getEstado());
+        Cliente cliente = new Cliente();
+        cliente.setData(Instant.now());
+        cliente.setNome(cadastroDTO.getNome());
+        cliente.setUsername(cadastroDTO.getUsername());
+        cliente.setTelefone(cadastroDTO.getTelefone());
+        cliente.setPassword(cadastroDTO.getPassword());
+        cliente.setEndereco(endereco);
+        return clienteRepository.save(cliente);
     }
 }
 
--- src/main/java/br/com/alexcosta/controllers/ProdutoControllers.java
+++ src/main/java/br/com/alexcosta/controllers/ProdutoControllers.java
@@ -2,23 +2,27 @@ package br.com.alexcosta.controllers;
 
 import br.com.alexcosta.dto.ProdutoDTO;
 
-import br.com.alexcosta.services.ProdutoServices;
+import br.com.alexcosta.entities.Produto;
+import br.com.alexcosta.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoControllers {
 
     @Autowired
-    private ProdutoServices produtoService;
+    private ProdutoRepository produtoRepository;
 
     @GetMapping(value="/listar")
     public List<ProdutoDTO> listarProdutos() {
-        return produtoService.buscarTodos();
+
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos.stream().map(ProdutoDTO::new).collect(Collectors.toList());
     }
 }
